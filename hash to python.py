c17d41bb536cc867ed2f299cae91663b658b4c1c
phemaq <39652738+phemaq@users.noreply.github.com>
2018-05-27 19:18:53 +0300

    blockchain

--- hash to python.py
+++ hash to python.py
@@ -20,7 +20,7 @@ class Block(object):
 
             def __str__(self):
                 return "Block<prev_hash: %s,hash: %s>" % (self.prev_hash, self.hash)
-
+ #создание блока 
 def create_first_block():
     block_data = {}
     block_data['index'] = 0
@@ -29,3 +29,28 @@ def create_first_block():
     block_data['prev_hash'] = None
     block = Block(block_data)
     return block
+
+    chaindata_dir = 'chaindata'
+    if not os.path.exists(chaindata_dir):
+        os.mkdir(chaindata_dir)
+    if os.listdir(chaindata_dir) == []:
+        first_block = create_first_block()
+        first_block.self_save()
+
+
+
+ #синхронизация блокчейна - локально
+        def sync():
+            node_blocks = []
+            chaindata_dir = 'chaindata'
+            if os.path.exists(chaindata_dir):
+                for filename in os.listdir(chaindata_dir):
+                    if filename.endswith('.json'):
+                        filepath = '%s/%s' % (chaindata_dir, filename)
+                        with open(filepath, 'r') as block_file:
+                            block_info = json.load(block_file)
+                            block_object = Block(block_info)
+
+                            node_blocks.append(block_object)
+                        return node_blocks
+                        
